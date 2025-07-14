967c61887f27a4d4fca8aff119878fee45ec86d4
Aviv Elany <132253879+AvivElany@users.noreply.github.com>
2025-07-14 09:33:20 +0300

    עדכון מסד נתונים ראשוני

---
+++ data/data.tsx
@@ -0,0 +1,40 @@
+/*
+{ hebrew: "", taatik: "", arabic: "", english: ""},
+ */
+
+
+export const vocabulary = [
+    { hebrew: "כסף", taatik: "מסארי", arabic: "", english: "money" },
+];
+
+export const arrest = [
+    { hebrew: "", taatik: "", arabic: "", english: "" },
+];
+
+export const suspect = [
+    { hebrew: "עצור!", taatik: "וקף!", arabic: "وقف!", english: "stop!" },
+    { hebrew: "עצור במקומך!", taatik: "וקף מחלכ!", arabic: "وقف محلك!", english: "Stop in your position!" },
+    { hebrew: "הרם ידיך!", taatik: "אירפע אידכ!", arabic: "يرفع يديك!", english: "Put your hands up!" },
+    { hebrew: "הוצא הכל מהכיסים!", taatik: "איטלע כל אישי מן ג'בע!", arabic: "", english: "Empty your pocketsTake everything out of your pockets!" },
+    { hebrew: "הרם ידיך!", taatik: "אירפע אידכ!", arabic: "", english: "Put your hands up!" },
+    { hebrew: "הרם חולצה!", taatik: "אירפע בלוזכ!", arabic: "", english: "Take off your shirt!" },
+    { hebrew: "הורד מכנסיים!", taatik: "נזל בנתלון!", arabic: "", english: "Take off your pants!" },
+    { hebrew: "הסתובב!", taatik: "ליף!", arabic: "", english: "Turn around!!" },
+    { hebrew: "הסתובב שוב פעם!", taatik: "ליף כמאן מרה!", arabic: "", english: "Turn around again!" },
+    { hebrew: "התלבש!", taatik: "אילבאס!", arabic: "", english: "Get dressed!" }
+]
+
+
+export const checkpost = [
+    { hebrew: "עצור!", taatik: "וקף!", arabic: "وقف!", english: "stop!" },
+    { hebrew: "עצור במקומך!", taatik: "וקף מחלכ!", arabic: "وقف محلك!", english: "Stop in your position!" },
+    { hebrew: "פתח את החלון!", taatik: "איפתח אל שבאכ!", arabic: "", english: "Open your window!" },
+    { hebrew: "הדלק את האור!", taatik: "אידווי א-דאו!", arabic: "", english: "Turn on the light!" },
+    { hebrew: "כבה מנוע!", taatik: "אידפי מוטור!", arabic: "وقف محلك!", english: "Stop your engine!" },
+    { hebrew: "הבא לי תעודה מזהה!", taatik: "העטיני הוויה!", arabic: "وقف محلك!", english: "Stop in your position!" },
+    { hebrew: "הבא לי תעודה מזהה!", taatik: "ג'יב אל הוויה!", arabic: "وقف محلك!", english: "Stop in your position!" },
+    { hebrew: "מאיפה אתה מגיע?", taatik: "מן ווין אינת בג'י?", arabic: "وقف محلك!", english: "Stop in your position!" },
+    { hebrew: "לאן אתה נוסע?", taatik: "לווין אינת סאפר?", arabic: "وقف محلك!", english: "Stop in your position!" },
+]
+
+
